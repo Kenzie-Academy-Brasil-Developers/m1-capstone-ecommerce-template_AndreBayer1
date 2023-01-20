47517e3a3c977e2597424a4d124a3fc3122673ab
AndreBayer <andrebayer1986@gmail.com>
2023-01-20 11:05:09 -0300

    Projeto Final M1 - modificacoes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 
 // ----------- Estrutura cabeçalho ----------- //
 
-
 let body = document.querySelector(".body");
 let header = document.createElement('header');
 header.innerHTML =
@@ -9,42 +8,39 @@ header.innerHTML =
 <h1 class="logoNome">WearTake</h1>
 <ul>
     <li class="botaoNav">
-        <button class="botaoTodos" href="#">Todos</button>
+        <button class="botaoTodos" id="todos">Todos</button>
     </li>
     <li class="botaoNav">
-        <button class="botaoAcessorios" href="#">Acessórios</button>
+        <button class="botaoAcessorios" id="acessorios">Acessórios</button>
     </li>
     <li class="botaoNav">
-        <button class="botaoCalcados" href="#">Calçados</button>
+        <button class="botaoCalcados" id="calcados">Calçados</button>
     </li>
     <li class="botaoNav">
-        <button class="botaoCamisetas" href="#">Camisetas</button>
+        <button class="botaoCamisetas" id="camisetas">Camisetas</button>
     </li>
 </ul>
 </nav>`
 
 body.insertAdjacentElement("afterbegin", header);
 
-
 // ----------- Estrutura Main ----------- //
 
-
-
 let main = document.querySelector(".alinhamento");
-let div1 = document.createElement('div');
-div1.innerHTML =
+let divVitrine = document.createElement('div');
+divVitrine.innerHTML =
     `<div>
 <ul class="vitrineProdutos">
 </ul>
 </div>`
 
 
-let div2 = document.createElement('div');
-div2.innerHTML =
+let divCarrinho = document.createElement('div');
+divCarrinho.innerHTML =
     `
     <div class="productCart">
 <div class="searchBar">
-    <input class="boxPesquisa" placeholder="O que deseja pesquisar?" type="text">
+    <input class="boxPesquisa" id="searchInput" placeholder="O que deseja pesquisar?" type="text">
     <button class="search">Pesquisar</button>
 </div>
 <span>Carrinho de compras</span>
@@ -66,60 +62,23 @@ div2.innerHTML =
 </div>
 </div>`;
 
-main.appendChild(div1);
-main.appendChild(div2);
-
-
-
-
-
-
-
-// ----------- Funcao cria card produtos  ----------- //
-
-
-function criaCard(data) {
-
-    let li = document.createElement("li");
-    li.classList.add("produto");
-
-
-    li.innerHTML =
-        `<figure>
-        <img src="${data.img}" alt="${data.nameItem}">
-        </figure>
-    <section class="infoProduto">
-        <h4 class="categoria">${data.tag}</h4>
-        <h3>${data.nameItem}</h3>
-        <p>${data.description}</p>
-        <h4 class="valor">R$ ${data.value}</h4>
-        <button class="addToCart" id=${data.id}>Adicionar ao carrinho</button>
-    </section>`;
-
-    return li;
-}
-
+main.appendChild(divVitrine);
+main.appendChild(divCarrinho);
 
 // ----------- Funcao Render => cria vitrine com produtos ----------- //
 
-
 let vitrineProdutos = document.querySelector(".vitrineProdutos")
 
-function listaVitrine(data, vitrineProdutos) {
-    for (let i = 0; i < data.length; i++) {
-        let produto = data[i];
-
+function listaVitrine(arr, parent) {
+    for (let i = 0; i < arr.length; i++) {
+        let produto = arr[i];
         let card = criaCard(produto);
-        vitrineProdutos.appendChild(card);
-
+        parent.appendChild(card);
     }
 }
 listaVitrine(data, vitrineProdutos);
 
-
-
-// ----------- funcao busca produto por id ----------- //
-
+// ----------- Funcao busca produto por id ----------- //
 
 function procuraProduto(id) {
     for (let i = 0; i < data.length; i++) {
@@ -130,58 +89,100 @@ function procuraProduto(id) {
     }
 }
 
-
-let cart = document.querySelector(".cart");
-let carrinhoVazio = document.querySelector(".emptyCart");
-let divCounterESoma = document.querySelector(".contadorESomaTotal");
-let productCart = document.querySelector(".productCart");
-
-let addToCartButton = document.querySelectorAll(".addToCart");
-let qtd = document.querySelector(".qtdProdutos");
-let total = document.querySelector(".somaTotalProdutos");
-
-
-
-// ----------- config botao adicionar ao carrinho  ----------- //
+// ----------- Config btn adicionar ao carrinho  ----------- //
 
 let quantidadeItens = 0;
 let valorTotal = 0;
+let qtd = document.querySelector(".qtdProdutos");
+let total = document.querySelector(".somaTotalProdutos");
+let divCounterESoma = document.querySelector(".contadorESomaTotal");
+let cart = document.querySelector(".cart");
+let productCart = document.querySelector(".productCart");
+let carrinhoVazio = document.querySelector(".emptyCart");
 
-function addCartBtn(addToCartButton) {
+function botaoAddCart(btn) {
 
-    for (let i = 0; i < addToCartButton.length; i++) {
-        let btn = addToCartButton[i];
-        btn.addEventListener("click", function (e) {
-            let elemento = e.target;
-            let idElemento = elemento.id;
-            let id = parseInt(idElemento);
-            let produto = procuraProduto(id);
+    btn.addEventListener("click", function (e) {
 
-            produtosNoCart(produto);
+        let elemento = e.target;
+        let idElemento = elemento.id;
+        let id = parseInt(idElemento);
+        let produto = procuraProduto(id);
+        let card = produtosNoCart(produto);
+        cart.appendChild(card);
 
-            quantidadeItens++;
-            qtd.innerText = quantidadeItens;
-            valorTotal += data[i].value;
-            total.innerText = `R$ ${valorTotal.toFixed(2)}`;
+        quantidadeItens++;
+        qtd.innerText = quantidadeItens;
 
+        let cartProducts = cart.children
+
+        if (cartProducts.length >= 1) {
+            cart.appendChild(carrinhoVazio);
+            divCounterESoma.remove(productCart);
             carrinhoVazio.remove();
-
-            if (cart.children.length === 1) {
-                divCounterESoma.style.visibility = 'visible';
-                productCart.appendChild(divCounterESoma);
-            }
-        });
-    }
+            divCounterESoma.style.visibility = 'visible';
+            productCart.appendChild(divCounterESoma);
+        }
+    })
 }
-addCartBtn(addToCartButton);
 
-// ----------- config dos itens no carrinho + config botao remover do carrinho ----------- //
+// ----------- Funcao cria card produtos ----------- //
+
+function criaCard(data) {
+    let li = document.createElement("li");
+    li.classList.add("produto");
+    li.id = data.id
+
+    let figure = document.createElement('figure');
+
+    let img = document.createElement('img');
+    img.src = `${data.img}`;
+    img.alt = `${data.nameItem}`
+
+    let section = document.createElement('section');
+    section.classList.add('infoProduto');
+
+    let h4 = document.createElement('h4');
+    h4.classList.add('categoria');
+    h4.innerText = data.tag
+
+    let h3 = document.createElement('h3');
+    h3.innerText = data.nameItem
+
+    let p = document.createElement('p');
+    p.innerText = data.description;
+
+    let valor = document.createElement('h4');
+    valor.classList.add('valor');
+    valor.innerText = `R$ ${data.value}`;
+
+    let btn = document.createElement('button');
+    btn.classList.add('addToCart');
+    btn.id = data.id
+    btn.innerText = `Adicionar ao carrinho`
+
+    botaoAddCart(btn)
+
+    li.appendChild(figure);
+    figure.appendChild(img);
+    li.appendChild(section);
+
+    section.appendChild(h4)
+    section.appendChild(h3)
+    section.appendChild(p)
+    section.appendChild(valor)
+    section.appendChild(btn)
+
+    return li;
+}
+
+// ----------- Itens no carrinho + Btn remover ----------- //
 
 
 function produtosNoCart(data) {
-
     let li = document.createElement("li");
     li.classList.add("inCartProduct");
+    li.id = 'f_' + data.id
 
     let figure = document.createElement("figure");
     let img = document.createElement("img");
@@ -198,29 +199,18 @@ function produtosNoCart(data) {
     let carrinhoPreco = document.createElement("p");
     carrinhoPreco.innerText = `R$ ${data.value}`;
 
-    let removerProduto = document.createElement("button");
-    removerProduto.classList.add("removerDoCarrinho");
-    removerProduto.innerText = "Remover do carrinho";
+    let btn = document.createElement("button");
+    btn.classList.add("removerDoCarrinho");
+    btn.innerText = "Remover do carrinho";
+    btn.id = 'cart_' + data.id
 
-    li.appendChild(figure);
-    li.appendChild(section);
-    figure.appendChild(img);
-
-    section.appendChild(carrinhoNome);
-    section.appendChild(carrinhoPreco);
-    section.appendChild(removerProduto);
-
-    cart.appendChild(li);
-
-
-    li.addEventListener("click", function () {
-
+    btn.addEventListener("click", function (e) {
         li.remove();
 
         quantidadeItens--;
-        valorTotal -= data.value;
+        //valorTotal -= data.value;
         qtd.innerText = quantidadeItens;
-        total.innerText = `R$ ${valorTotal.toFixed(2)}`;
+        //total.innerText = `R$ ${valorTotal.toFixed(2)}`;
 
         let cartProducts = cart.children
 
@@ -229,56 +219,72 @@ function produtosNoCart(data) {
             divCounterESoma.remove(productCart);
         }
     })
+
+    li.appendChild(figure);
+    li.appendChild(section);
+    figure.appendChild(img);
+
+    section.appendChild(carrinhoNome);
+    section.appendChild(carrinhoPreco);
+    section.appendChild(btn);
+
+    return li;
+
 };
 
+// ----------- Filtro Search ----------- //
 
-// ----------- Config Filtro Botoes de Navegacao ----------- //
+let searchButton = document.querySelector('.search');
+searchButton.addEventListener('click', searchProducts);
 
-
-
-function filtroData(data, vitrineProdutos) {
-
-
-    let camisetas = [];
-    let acessorios = [];
-    let calcados = [];
-
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].tag[0].toLowerCase() == "camisetas") {
-            camisetas.push(data[i]);
-        } else if (data[i].tag[0].toLowerCase() == "acessórios") {
-            acessorios.push(data[i]);
-        } else if (data[i].tag[0].toLowerCase() == "calçados") {
-            calcados.push(data[i]);
-        } else { }
-    }
-
-    let botaoCamisetas = document.querySelector(".botaoCamisetas");
-    botaoCamisetas.addEventListener("click", function () {
-
-        vitrineProdutos.innerHTML = ""; listaVitrine(camisetas, vitrineProdutos);
-    })
-
-    let botaoAcessorios = document.querySelector(".botaoAcessorios");
-    botaoAcessorios.addEventListener("click", function () {
-
-        vitrineProdutos.innerHTML = "", listaVitrine(acessorios, vitrineProdutos);
-    })
-
-    let botaoCalcados = document.querySelector(".botaoCalcados");
-    botaoCalcados.addEventListener("click", function () {
-
-        vitrineProdutos.innerHTML = ""; listaVitrine(calcados, vitrineProdutos);
-    })
-
-    let botaoTodos = document.querySelector(".botaoTodos");
-    botaoTodos.addEventListener("click", function () {
-        vitrineProdutos.innerHTML = '', listaVitrine(data, vitrineProdutos);
-    })
-
+function searchProducts() {
+    let searchInput = document.querySelector('#searchInput').value;
+    let filteredProducts = data.filter(product => product.nameItem.toLowerCase().includes(searchInput.toLowerCase()));
+    vitrineProdutos.innerHTML = '';
+    listaVitrine(filteredProducts, vitrineProdutos);
 }
-filtroData(data, vitrineProdutos)
 
 
+// ----------- Filtro Botoes de Navegacao ----------- //
 
+let camisetas = [];
+let acessorios = [];
+let calcados = [];
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i].tag[0].toLowerCase() == "camisetas") {
+        camisetas.push(data[i]);
+    } else if (data[i].tag[0].toLowerCase() == "acessórios") {
+        acessorios.push(data[i]);
+    } else if (data[i].tag[0].toLowerCase() == "calçados") {
+        calcados.push(data[i]);
+    }
+}
+
+console.log(camisetas);
+console.log(acessorios);
+
+let botaoCamisetas = document.querySelector("#camisetas");
+botaoCamisetas.addEventListener("click", function () {
+
+    vitrineProdutos.innerHTML = ""; listaVitrine(camisetas, vitrineProdutos);
+})
+
+let botaoAcessorios = document.querySelector("#acessorios");
+botaoAcessorios.addEventListener("click", function () {
+
+    vitrineProdutos.innerHTML = "", listaVitrine(acessorios, vitrineProdutos);
+})
+
+let botaoCalcados = document.querySelector("#calcados");
+botaoCalcados.addEventListener("click", function () {
+
+    vitrineProdutos.innerHTML = ""; listaVitrine(calcados, vitrineProdutos);
+})
+
+let botaoTodos = document.querySelector("#todos");
+botaoTodos.addEventListener("click", function () {
+
+    vitrineProdutos.innerHTML = ""; listaVitrine(data, vitrineProdutos);
+})
 
